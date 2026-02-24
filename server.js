c46672dcf3a8d49ae15c3c07191a54d9a61cd6ec
cjdreamy <josephalocye402@gmail.com>
2026-02-24 13:22:31 +0300

    verbose importing

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./myusers.db', (err) =>{
     if(err){
         return console.log(err)
