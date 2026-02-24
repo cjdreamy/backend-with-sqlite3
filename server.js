04791e2759efc3da850a934a3f5ae6b79c8d2da1
cjdreamy <josephalocye402@gmail.com>
2026-02-24 13:02:46 +0300

    created users table

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const db = new sqlite3.Database('./myusers.db', (err) =>{
     console.log("connection etablished successfully")
 });
 
+//CREATING USERTABLE
+db.run(`
+    CREATE TABLE IF NOT EXISTS users(
+    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+    userName TEXT NOT NULL,
+    email TEXT NOT NULL,
+    passWord TEXT NOT NULL)`
+)
+
 db.close((err) => {
     if(err){
         return console.log(err)
